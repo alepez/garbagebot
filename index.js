d1391209830cb4e9fc4069a63e2b928e866c0851
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2018-05-19 00:22:50 +0200

    Add Subscriber

--- index.js
+++ index.js
@@ -1,10 +1,41 @@
 const Bot = require('node-telegram-bot-api');
+const fs = require('fs');
+const path = require('path');
 
 const token = process.env.TELEGRAM_TOKEN;
+const dbDir = process.env.GARBAGEBOT_DB_DIR;
 const bot = new Bot(token, { polling: true });
 
-bot.on('message', (msg) => {
-  const text = msg.text.toString();
-  const chatId = msg.chat.id;
-  bot.sendMessage(msg.chat.id, msg.text.toString().toUpperCase());
+const subscribers = [];
+
+const Subscriber = function (msg) {
+  console.log(msg);
+  const { chat } = msg;
+  const filename = path.join(dbDir, String(chat.id));
+
+  this.data = {
+    chat
+  };
+
+  this.save = () => {
+    console.log(filename);
+    return new Promise((resolve, reject) => {
+      fs.writeFile(filename, JSON.stringify(this.data, null, 2), (err) => {
+        if (err) return reject();
+        return resolve();
+      });
+    });
+  };
+};
+
+bot.on('message', async (msg) => {
+  const { text, chat } = msg;
+
+  console.log(JSON.stringify(msg));
+
+  if (text === '/start') {
+    const sub = new Subscriber(msg);
+    subscribers[chat.id] = sub;
+    await sub.save();
+  }
 });
