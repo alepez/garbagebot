1c2d42bac6108edc8bc073624e21040c3818c19a
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2018-05-19 00:40:37 +0200

    Permanent storage

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const Bot = require('node-telegram-bot-api');
 const fs = require('fs');
 const path = require('path');
+const _ = require('lodash');
 
 const token = process.env.TELEGRAM_TOKEN;
 const dbDir = process.env.GARBAGEBOT_DB_DIR;
 const bot = new Bot(token, { polling: true });
-
-const subscribers = [];
 
 const Subscriber = function (msg) {
   console.log(msg);
@@ -28,12 +27,37 @@ const Subscriber = function (msg) {
   };
 };
 
+const listAllFiles = () => {
+  return fs.readdirSync(dbDir);
+};
+
+const readFile = (filename) => {
+  return fs.readFileSync(path.join(dbDir, filename), 'utf8');
+}
+
+const readAllFiles = () => {
+  const files = listAllFiles();
+  const records = {};
+  files.forEach(filename => {
+    records[filename] = JSON.parse(readFile(filename));
+  });
+  return records;
+};
+
+const subscribers = readAllFiles();
+console.log(subscribers);
+
 bot.on('message', async (msg) => {
   const { text, chat } = msg;
 
   console.log(JSON.stringify(msg));
 
   if (text === '/start') {
+    if (subscribers[chat.id]) {
+      bot.sendMessage(chat.id, "Already registered");
+      return;
+    }
+
     const sub = new Subscriber(msg);
     subscribers[chat.id] = sub;
     await sub.save();
